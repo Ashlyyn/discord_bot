a9c07fc4db27a88b40c1fb42440e0181fde3354c
Fingerlingus <sirfingerlingus@gmail.com>
2021-03-14 00:30:04 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 class MyClientClass : public SleepyDiscord::DiscordClient {
 public:
 	using SleepyDiscord::DiscordClient::DiscordClient;
+
 	void onMessage(SleepyDiscord::Message message) override {
 		if (message.startsWith(prefix + "prefix ")) {
 			changePrefix(message.content.substr(prefix.size() + strlen("prefix ")));
@@ -15,8 +16,24 @@ public:
 			echo(message.channelID, message.content.substr(prefix.size() + strlen("echo ")));
 		}
 
+		else if (message.startsWith(prefix + "mute voice ")) {
+			muteVoice(message.serverID, message.content.substr(prefix.size() + strlen("mute voice ")));
+		}
+
+		else if (message.startsWith(prefix + "unmute voice")) {
+			unmuteVoice(message.serverID, message.content.substr(prefix.size() + strlen("unmute voice ")));
+		}
+
+		else if (message.startsWith(prefix + "mute text ")) {
+			muteText(message.serverID, getSnowflake(message.content.substr(prefix.size() + strlen("mute text "))));
+		}
+
+		else if (message.startsWith(prefix + "unmute text ")) {
+			unmuteText(message.serverID, getSnowflake(message.content.substr(prefix.size() + strlen("unmute text "))));
+		}
+
 		else if (message.startsWith(prefix + "logs ")) {
-			setLogsChannel(SleepyDiscord::Snowflake<SleepyDiscord::Channel>(std::string(message.content.substr(prefix.size() + strlen("logs ")))));
+			setLogsChannel(SleepyDiscord::Snowflake<SleepyDiscord::Channel>(getSnowflake(message.content.substr(prefix.size() + strlen("logs ")))));
 			logAction("Logs enabled in " + message.content.substr(prefix.size() + strlen("logs ")));
 		}
 
@@ -34,14 +51,57 @@ public:
 			logAction(log);
 		}
 
+		else if (message.startsWith(prefix + "permissions set ")) {
+			auto words = split(message.content);
+			std::map<std::string, COMMAND_PERMISSION> perms = {
+				{ std::string("owner_only"), COMMAND_PERMISSION::OWNER_ONLY },
+				{ std::string("no_owner"), COMMAND_PERMISSION::NO_OWNER },
+				{ std::string("bot_admin"), COMMAND_PERMISSION::BOT_ADMIN },
+				{ std::string("all"), COMMAND_PERMISSION::CMD_ALL }
+			};
+			std::map<std::string, COMMAND_TYPE> type = {
+				{ std::string("admin"), COMMAND_TYPE::ADMIN },
+				{ std::string("non_admin"), COMMAND_TYPE::NON_ADMIN },
+				{ std::string("all"), COMMAND_TYPE::ROLE_ALL }
+			};
+			setPermissions(perms[words[2]], type[words[3]]);
+			logAction("**PERMISSIONS CHANGE:**\n```Type: " + words[3] + "\nSet to: " + words[2] + "\nSet by: " + message.author.username + "#" + message.author.discriminator + "```\n");
+		}
+
 		else if (message.startsWith(prefix + "die")) {
-			die(message.channelID);
+			if(checkPermissions(message.author, ADMIN) == true) {
+				die(message.channelID);
+			}
+		}
+
+		else if(message.startsWith(prefix)) {
+			sendMessage(message.channelID, "Unknown command.");
+		}
+
+		if(isMuted(message.author.ID)) {
+			deleteMessage(message.channelID, message.ID);
 		}
 	}
 
-private:
+protected:
 	std::string prefix = "F!";
 	SleepyDiscord::Snowflake<SleepyDiscord::Channel> logsChannel;
+	std::vector<SleepyDiscord::Snowflake<SleepyDiscord::User> > mutedUsers;
+
+	enum COMMAND_PERMISSION {
+		OWNER_ONLY,
+		NO_OWNER,
+		BOT_ADMIN,
+		CMD_ALL
+	};
+
+	enum COMMAND_TYPE {
+		ADMIN,
+		NON_ADMIN,
+		ROLE_ALL
+	};
+
+	std::array<int, 2> permissions;
 
 	void changePrefix(const std::string& newPrefix) {
 		prefix = newPrefix;
@@ -55,6 +115,26 @@ private:
 		sendMessage(channel, message);
 	}
 
+	void muteVoice(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& server, const SleepyDiscord::Snowflake<SleepyDiscord::User>& user) {
+		muteServerMember(server, user, true);
+	}
+
+	void unmuteVoice(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& server, const SleepyDiscord::Snowflake<SleepyDiscord::User>& user) {
+		muteServerMember(server, user, false);
+	}
+
+	void muteText(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& server, const SleepyDiscord::Snowflake<SleepyDiscord::User> user) {
+		if(std::count(mutedUsers.begin(), mutedUsers.end(), user) == 0) {
+			mutedUsers.push_back(user);
+		}
+	}
+
+	void unmuteText(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& server, const SleepyDiscord::Snowflake<SleepyDiscord::User> user) {
+		if(std::count(mutedUsers.begin(), mutedUsers.end(), user) > 0) {
+			mutedUsers.erase(std::find(mutedUsers.begin(), mutedUsers.end(), user));
+		}
+	}
+
 	void setLogsChannel(const SleepyDiscord::Snowflake<SleepyDiscord::Channel>& channel) {
 		logsChannel = channel;
 	}
@@ -63,11 +143,22 @@ private:
 		sendMessage(logsChannel, str);
 	}
 
+	void setPermissions(COMMAND_PERMISSION commandPerm, COMMAND_TYPE commandType) {
+		if(commandType == ROLE_ALL) {
+			permissions[COMMAND_TYPE::ADMIN] = commandPerm;
+			permissions[COMMAND_TYPE::NON_ADMIN] = commandPerm;
+		} else {
+			permissions[commandType] = commandPerm;
+		}
+	}
+
 	void die(const SleepyDiscord::Snowflake<SleepyDiscord::Channel> channel) {
 		sendMessage(channel, "Okay.");
 		exit(0);
 	}
 
+
+private:
 	std::vector<std::string> split(std::string string) {
 		std::vector<std::string> vec;
 		int pos = 0;
@@ -90,9 +181,44 @@ private:
 		}
 		return vec;
 	}
+
+	std::string getSnowflake(const std::string str) {
+		if(str.find("<@!") != std::string::npos) {
+			return str.substr(3, str.size() - 3 - 1);
+		}
+		else if(str.find("<#") != std::string::npos) {
+			return str.substr(2, str.size() - 2 - 1);
+		}
+		else throw std::runtime_error("getSnowflake: string is not a server or channel mention.");
+	}
+
+	bool isMuted(const SleepyDiscord::Snowflake<SleepyDiscord::User>& user) {
+		if(std::count(mutedUsers.begin(), mutedUsers.end(), user) >= 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	bool checkPermissions(SleepyDiscord::Snowflake<SleepyDiscord::User>& user, COMMAND_TYPE commandType) {
+		if(permissions[commandType] == OWNER_ONLY && user != 518216114665291786) {
+			return false;
+		} 
+		else if(permissions[commandType] == NO_OWNER && user == 518216114665291786) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 };
 
 int main() {
 	MyClientClass client("NzgzMTc3ODExOTUwOTYwNjcw.X8W9LQ.E3Kusm_bV5g8KbDzTymcQjobPVM", SleepyDiscord::USER_CONTROLED_THREADS);
-	client.run();
+	try{
+		client.run();
+	} catch(std::runtime_error& e) {
+		std::fprintf(stderr, "%s\n", e.what());
+		return 1;
+	}
 }
