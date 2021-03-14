85f94bef02eeddfea2da9740fff28d3ee8824438
Fingerlingus <sirfingerlingus@gmail.com>
2021-03-14 09:44:21 -0600

    changes

--- main.cpp
+++ main.cpp
@@ -6,86 +6,94 @@ public:
 
 	void onMessage(SleepyDiscord::Message message) override {
 		if (message.startsWith(prefix + "prefix ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				changePrefix(message.content.substr(prefix.size() + strlen("prefix ")));
 			}
 		}
 		else if (message.startsWith(prefix + "hello")) {
-			if(checkPermissions(message.author, NON_ADMIN) == true) {
+			if(checkPermissions(message.member, NON_ADMIN) == true) {
 				hello(message.channelID, message.author);
 			}
 		}
 
 		else if (message.startsWith(prefix + "echo ")) {
-			if(checkPermissions(message.author, NON_ADMIN) == true) {
+			if(checkPermissions(message.member, NON_ADMIN) == true) {
 				echo(message.channelID, message.content.substr(prefix.size() + strlen("echo ")));
 			}
 		}
 
 		else if (message.startsWith(prefix + "mute voice ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				muteVoice(message.serverID, message.content.substr(prefix.size() + strlen("mute voice ")));
 			}
 		}
 
-		else if (message.startsWith(prefix + "unmute voice")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+		else if (message.startsWith(prefix + "unmute voice ")) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				unmuteVoice(message.serverID, message.content.substr(prefix.size() + strlen("unmute voice ")));
 			}
 		}
 
 		else if (message.startsWith(prefix + "mute text ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				muteText(message.serverID, getSnowflake(message.content.substr(prefix.size() + strlen("mute text "))));
 			}
 		}
 
 		else if (message.startsWith(prefix + "unmute text ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				unmuteText(message.serverID, getSnowflake(message.content.substr(prefix.size() + strlen("unmute text "))));
 			}
 		}
 
 
+		else if (message.startsWith(prefix + "bot_admin_role set ")) {
+			if (checkPermissions(message.member, ADMIN) == true) {
+				auto words = split(message.content);
+				setBotAdminRole(getSnowflake(message.content.substr(prefix.size() + strlen("bot_admin_role set "))));
+				logAction("**BOT ADMIN ROLE SET**\n```Set to: " + words[2] + "\nSet by: " + message.author.username + "#" + message.author.discriminator + "\n```");
+			}
+		}
+
 		else if (message.startsWith(prefix + "nologs")) {
-			if (checkPermissions(message.author, ADMIN) == true) {
+			if (checkPermissions(message.member, ADMIN) == true) {
 				logsDisable();
 			}
 		}
 
 		else if (message.startsWith(prefix + "logs disable")) {
-			if (checkPermissions(message.author, ADMIN) == true) {
+			if (checkPermissions(message.member, ADMIN) == true) {
 				logsDisable();
 			}
 		}
 
 		else if (message.startsWith(prefix + "logs enable")) {
-			if (checkPermissions(message.author, ADMIN) == true) {
+			if (checkPermissions(message.member, ADMIN) == true) {
 				logsDisable(false);
 			}
 		}
 
 		else if (message.startsWith(prefix + "logs ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				setLogsChannel(SleepyDiscord::Snowflake<SleepyDiscord::Channel>(getSnowflake(message.content.substr(prefix.size() + strlen("logs ")))));
 				logAction("Logs enabled in " + message.content.substr(prefix.size() + strlen("logs ")));
 			}
 		}
 		
 		else if (message.startsWith(prefix + "silent")) {
-			if (checkPermissions(message.author, ADMIN) == true) {
+			if (checkPermissions(message.member, ADMIN) == true) {
 				setSilent();
 			}
 		}
 
 		else if (message.startsWith(prefix + "nosilent")) {
-			if (checkPermissions(message.author, ADMIN) == true) {
+			if (checkPermissions(message.member, ADMIN) == true) {
 				setSilent(false);
 			}
 		}
 
 		else if (message.startsWith(prefix + "delete ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				std::string log;
 				std::vector<std::string> words = split(message.content);
 				if (words.size() > 2) {
@@ -101,11 +109,10 @@ public:
 		}
 
 		else if (message.startsWith(prefix + "permissions set ")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				auto words = split(message.content);
 				std::map<std::string, COMMAND_PERMISSION> perms = {
 					{ std::string("owner_only"), COMMAND_PERMISSION::OWNER_ONLY },
-					{ std::string("no_owner"), COMMAND_PERMISSION::NO_OWNER },
 					{ std::string("bot_admin"), COMMAND_PERMISSION::BOT_ADMIN },
 					{ std::string("all"), COMMAND_PERMISSION::CMD_ALL }
 				};
@@ -114,13 +121,13 @@ public:
 					{ std::string("non_admin"), COMMAND_TYPE::NON_ADMIN },
 					{ std::string("all"), COMMAND_TYPE::ROLE_ALL }
 				};
-				setPermissions(perms[words[2]], type[words[3]]);
+				setPermissions(perms[words[3]], type[words[2]]);
 				logAction("**PERMISSIONS CHANGE:**\n```Type: " + words[3] + "\nSet to: " + words[2] + "\nSet by: " + message.author.username + "#" + message.author.discriminator + "```\n");
 			}
 		}
 
 		else if (message.startsWith(prefix + "die")) {
-			if(checkPermissions(message.author, ADMIN) == true) {
+			if(checkPermissions(message.member, ADMIN) == true) {
 				die(message.channelID);
 			}
 		}
@@ -139,11 +146,11 @@ protected:
 	bool noLogs = false;
 	std::string prefix = "F!";
 	SleepyDiscord::Snowflake<SleepyDiscord::Channel> logsChannel;
+	SleepyDiscord::Snowflake<SleepyDiscord::Role> botAdminRole;
 	std::vector<SleepyDiscord::Snowflake<SleepyDiscord::User> > mutedUsers;
 
 	enum COMMAND_PERMISSION {
 		OWNER_ONLY,
-		NO_OWNER,
 		BOT_ADMIN,
 		CMD_ALL
 	};
@@ -154,7 +161,9 @@ protected:
 		ROLE_ALL
 	};
 
-	std::array<int, 2> permissions;
+	std::array<int, 2> permissions = {
+		COMMAND_PERMISSION::CMD_ALL, COMMAND_PERMISSION::CMD_ALL
+	};
 
 	void changePrefix(const std::string& newPrefix) {
 		prefix = newPrefix;
@@ -188,6 +197,10 @@ protected:
 		if(std::count(mutedUsers.begin(), mutedUsers.end(), user) > 0) {
 			mutedUsers.erase(std::find(mutedUsers.begin(), mutedUsers.end(), user));
 		}
+	}
+
+	void setBotAdminRole(const SleepyDiscord::Snowflake<SleepyDiscord::Role>& role) {
+		botAdminRole = role;
 	}
 
 	void setLogsChannel(const SleepyDiscord::Snowflake<SleepyDiscord::Channel>& channel) {
@@ -256,7 +269,10 @@ private:
 		else if(str.find("<#") != std::string::npos) {
 			return str.substr(2, str.size() - 2 - 1);
 		}
-		else throw std::runtime_error("getSnowflake: string is not a server or channel mention.");
+		else if (str.find("<@&") != std::string::npos) {
+			return str.substr(3, str.size() - 3 - 1);
+		}
+		else throw std::runtime_error("getSnowflake: string is not a member, role, or channel mention.");
 	}
 
 	bool isMuted(const SleepyDiscord::Snowflake<SleepyDiscord::User>& user) {
@@ -267,15 +283,37 @@ private:
 		}
 	}
 
-	bool checkPermissions(SleepyDiscord::Snowflake<SleepyDiscord::User>& user, COMMAND_TYPE commandType) {
-		if(permissions[commandType] == OWNER_ONLY && user != 518216114665291786) {
-			return false;
-		} 
-		else if(permissions[commandType] == NO_OWNER && user == 518216114665291786) {
-			return false;
+	bool hasRole(const SleepyDiscord::ServerMember& member, const SleepyDiscord::Snowflake<SleepyDiscord::Role>& role) {
+		bool b = false;
+		for (const auto& memberRole : member.roles) {
+			if (memberRole == role) {
+				b = true;
+			}
+		}
+		return b;
+	}
+
+	bool isOwner(const SleepyDiscord::Snowflake<SleepyDiscord::User>& user) {
+		if (user == 518216114665291786) {
+			return true;
 		}
 		else {
+			return false;
+		}
+	}
+
+	bool checkPermissions(const SleepyDiscord::ServerMember& member, COMMAND_TYPE commandType) {
+		if(permissions[commandType] == OWNER_ONLY && isOwner(member.user.ID)) {
 			return true;
+		} 
+		else if ((permissions[commandType] == BOT_ADMIN && hasRole(member, botAdminRole)) || isOwner(member.user.ID)) {
+			return true;
+		}
+		else if (permissions[commandType] == CMD_ALL) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 };
