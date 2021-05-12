d5c59b99862625ada2ca8a9d5237725bbc69daf1
Fingerlingus <sirfingerlingus@gmail.com>
2021-05-12 17:17:32 -0600

    moved isOwner() and hasRole() to bot client class

--- command.cpp
+++ command.cpp
@@ -3,37 +3,18 @@
 
 #include "sleepy_discord/sleepy_discord.h"
 
-bool Command::isOwner(const SleepyDiscord::Snowflake<SleepyDiscord::User>& acrUserID) {
-	if (acrUserID == MyClientClass::s_ownerID) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-bool Command::hasRole(SleepyDiscord::Server& arServer, const SleepyDiscord::Snowflake<SleepyDiscord::User>& acrUserID, const SleepyDiscord::Snowflake<SleepyDiscord::Role>& acrRoleID) {
-	const SleepyDiscord::ServerMember& lcrMember = *arServer.findMember(acrUserID);
-	for (const auto& lMemberRoleID : lcrMember.roles) {
-	    if (lMemberRoleID == acrRoleID) {
-            return true;
-		}
-	}
-	return false;
-}
-
 bool Command::checkPermissions(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& acrServerID, const SleepyDiscord::User& acrUser) const {
 	std::mutex mutex;
 	std::lock_guard lock(mutex);
 
-	assert(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] != MyClientClass::COMMAND_PERMISSION::ERR);
+	assert(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] != MyClientClass::COMMAND_PERMISSION::PERM_ERR);
 
 	// if server's permissions for command's command type is owner only and user is owner
-	if(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::OWNER_ONLY && isOwner(acrUser.ID)) {
+	if(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::OWNER_ONLY && MyClientClass::isOwner(acrUser.ID)) {
     	return true;
 	} 
 	// else if server's permissions for command's command type is set to admin role and user has role
-    else if (((m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (hasRole(m_client->m_servers.at(acrServerID), acrUser.ID, m_client->m_serverBotSettings.at(acrServerID).botAdminRoleID))) || isOwner(acrUser.ID)) {
+    else if (((m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (m_client->hasRole(m_client->m_servers.at(acrServerID), acrUser.ID, m_client->m_serverBotSettings.at(acrServerID).botAdminRoleID))) || MyClientClass::isOwner(acrUser.ID)) {
 		return true;
     }
 	// else if server's permissions for command's command type is set to all
