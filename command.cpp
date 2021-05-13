fa320fa1c5241d98ee1924f08c99ab0c8a5ddb60
Fingerlingus <sirfingerlingus@gmail.com>
2021-05-13 11:32:20 -0600

    replaced call of m_client->getUser() with m_client->m_cache.getUser()

--- command.cpp
+++ command.cpp
@@ -15,7 +15,7 @@ bool Command::checkPermissions(const SleepyDiscord::Snowflake<SleepyDiscord::Ser
     	return true;
 	} 
 	// else if server's permissions for command's command type is set to admin role and user has role
-    else if (((m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (m_client->hasRole(m_client->m_servers.at(acrServerID), acrUserID, m_client->m_serverBotSettings.at(acrServerID).botAdminRoleID))) || MyClientClass::isOwner(acrUserID)) {
+    else if (((m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (m_client->hasRole(m_client->m_cache.getServer(acrServerID), acrUserID, m_client->m_serverBotSettings.at(acrServerID).botAdminRoleID))) || MyClientClass::isOwner(acrUserID)) {
 		return true;
     }
 	// else if server's permissions for command's command type is set to all
