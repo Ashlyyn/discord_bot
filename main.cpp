3f31c0756e866ee81f77ba363b73ebea4dbeff11
Fingerlingus <sirfingerlingus@gmail.com>
2021-05-12 18:31:31 -0600

    added grabbing of ownerID from config.json, added error checking for grabbing both ownerID and token vals

--- main.cpp
+++ main.cpp
@@ -6,20 +6,37 @@ int main() {
 	std::string lContent(std::istreambuf_iterator<char>(lConfigFile), (std::istreambuf_iterator<char>()));
 	rapidjson::Document lDoc;
 	lDoc.Parse(lContent.c_str());
-	rapidjson::Value& lToken = lDoc["token"];
+	rapidjson::Value& lTokenVal = lDoc["token"];
+	rapidjson::Value& lOwnerIDVal = lDoc["ownerID"];
+	try {
+		if(lTokenVal.IsNull()) {
+			throw std::runtime_error("config.json token field null, exiting");
+		} else if(!lTokenVal.IsString()) {
+			throw std::runtime_error("config.json token field invalid data, exiting");
+		}
+
+		if(lOwnerIDVal.IsNull()) {
+			throw std::runtime_error("config.json ownerID field null, exiting");
+		} else if(!lOwnerIDVal.IsString()) {
+			throw std::runtime_error("config.json ownerID field invalid data, exiting");
+		}
+	} catch(const std::runtime_error& e) {
+		std::fprintf(stderr, "main(): error reading config info: %s\n", e.what());
+		return 3;
+	}
 
 	try {
-		MyClientClass client(lToken.GetString(), SleepyDiscord::USER_CONTROLED_THREADS);
+		MyClientClass::s_ownerID = std::string(lOwnerIDVal.GetString());
+		MyClientClass client(lTokenVal.GetString(), SleepyDiscord::USER_CONTROLED_THREADS);
 		try {
 			client.init();
 			client.run();
-		}
-		catch (std::runtime_error& e) {
+		} catch (std::runtime_error& e) {
 			std::fprintf(stderr, "main(): uncaught exception: %s\n", e.what());
 			return 1;
 		}
-	}
-	catch (...) {
-		std::fprintf(stderr, "main(): error running bot: %s\n", lToken.GetString());
+	} catch (...) {
+		std::fprintf(stderr, "main(): error running bot: %s\n", lTokenVal.GetString());
+		return 2;
 	}
 }
