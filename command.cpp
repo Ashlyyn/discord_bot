d6d641e9992e2e92e3f584830cba31f1d2db5811
Fingerlingus <sirfingerlingus@gmail.com>
2021-06-28 04:29:58 -0600

    updated with calls to cache

--- command.cpp
+++ command.cpp
@@ -7,19 +7,19 @@ bool Command::checkPermissions(const SleepyDiscord::Snowflake<SleepyDiscord::Ser
 	std::mutex mutex;
 	std::lock_guard lock(mutex);
 
-	assert(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] != MyClientClass::COMMAND_PERMISSION::PERM_ERR);
+	assert(m_client->m_cache.getServerBotSettings(acrServerID).permissions[m_commandType] != MyClientClass::COMMAND_PERMISSION::PERM_ERR);
 
 	SleepyDiscord::User lUser = m_client->getUser(acrUserID);
 	// if server's permissions for command's command type is owner only and user is owner
-	if(m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::OWNER_ONLY && MyClientClass::isOwner(acrUserID)) {
+	if(m_client->m_cache.getServerBotSettings(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::OWNER_ONLY && MyClientClass::isOwner(acrUserID)) {
     	return true;
 	} 
 	// else if server's permissions for command's command type is set to admin role and user has role
-    else if (((m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (m_client->hasRole(m_client->m_cache.getServer(acrServerID), acrUserID, m_client->m_serverBotSettings.at(acrServerID).botAdminRoleID))) || MyClientClass::isOwner(acrUserID)) {
+    else if (((m_client->m_cache.getServerBotSettings(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::BOT_ADMIN) && (m_client->hasRole(m_client->m_cache.getServer(acrServerID), acrUserID, m_client->m_cache.getServerBotSettings(acrServerID).botAdminRoleID))) || MyClientClass::isOwner(acrUserID)) {
 		return true;
     }
 	// else if server's permissions for command's command type is set to all
-    else if (m_client->m_serverBotSettings.at(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::CMD_ALL) {
+    else if (m_client->m_cache.getServerBotSettings(acrServerID).permissions[m_commandType] == MyClientClass::COMMAND_PERMISSION::CMD_ALL) {
     	return true;
 	}
     else {
