d73fd38ef3a5d74ebe55aa327848762062cead6a
Fingerlingus <sirfingerlingus@gmail.com>
2021-06-30 03:53:15 -0600

    moved hashes to dedicated header file

--- server_cache.hpp
+++ server_cache.hpp
@@ -5,30 +5,7 @@
 #include "sleepy_discord/sleepy_discord.h"
 
 #include "server_bot_settings.hpp"
-
-struct UserIDHash {
-    std::size_t operator()(const SleepyDiscord::Snowflake<SleepyDiscord::User>& acrUserID) const {
-        return std::hash<std::string>()(acrUserID);
-    }
-};
-
-struct UserHash {
-    std::size_t operator()(const SleepyDiscord::User& acrUser) const {
-        return std::hash<std::string>()(acrUser.ID);
-    }
-};
-
-struct ServerIDHash {
-    std::size_t operator()(const SleepyDiscord::Snowflake<SleepyDiscord::Server>& acrServerID) const {
-        return std::hash<std::string>()(acrServerID);
-    }
-};
-
-struct ServerHash {
-    std::size_t operator()(const SleepyDiscord::Server& acrServer) const {
-        return std::hash<std::string>()(acrServer.ID);
-    }
-};
+#include "hash.hpp"
 
 class ServerCache {
 public:
